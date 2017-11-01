02f8ac9962307333df1c39084e42a2224667ac80
cenv0594 <gwyneth.bradbury@ouce.ox.ac.uk>
2017-11-01 16:47:39 +0000

    changepassword still not working

--- it_monitor_app/views.py
+++ it_monitor_app/views.py
@@ -46,7 +46,7 @@ def wakeonlan():
 @app.route('/changepasswd', methods=["GET", "POST"])
 def changepasswd():
     import auth.iaasldap as auth
-    auth.change_passwordAD(user=request.form.get('username'),
+    auth.change_password(user=request.form.get('username'),
                            current_pass=request.form.get('current_pass'),
                            new_pass=request.form.get('new_pass'),
                            repeat_password=request.form.get('rep_pass'))
