03f87e20a8d903b2a3bec4e1f4da94b6d8001e4e
cenv0594 <gwyneth.bradbury@ouce.ox.ac.uk>
2018-09-19 15:10:43 +0100

    tidying for ipad

--- it_monitor_app/views.py
+++ it_monitor_app/views.py
@@ -269,7 +269,7 @@ def changepasswd():
         else:
             flash("Failed to Change Password", 'error')
         return render_template('changepasswd.html', form=form)
-    return "derp"
+    # return "derp"
     return render_template('changepasswd.html', form=form)
 
 
